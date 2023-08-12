60dae3c68c90b44032b33fe1674dc00d8dbfcc58
Igor Bibikov <igb604@yandex.ru>
2023-08-12 13:31:42 +0500

    фронт+бэк 10

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const { PORT, DB_ADDRESS } = process.env;
-const cookies = require('cookie-parser');
+const cookies = require("cookie-parser");
 
-const { errors } = require('celebrate');
-const cors = require('cors');
-const { routes } = require('./routes/index');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errors } = require("celebrate");
+const cors = require("cors");
+const { routes } = require("./routes/index");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const errorHandler = require('./middlewares/error-handler');
+const errorHandler = require("./middlewares/error-handler");
 
 // Запуск приложения
 const app = express();
@@ -29,16 +29,16 @@ app.use(cookies());
 app.use(
   cors({
     origin: [
-      'https://praktikum.tk',
-      'http://praktikum.tk',
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:4000',
-      'http://igorb.nomoreparties.co',
-      'https://igorb.nomoreparties.co',
+      "https://praktikum.tk",
+      "http://praktikum.tk",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:4000",
+      "http://igorb.nomoreparties.co",
+      "https://igorb.nomoreparties.co",
     ],
     credentials: true,
-  }),
+  })
 );
 // подключаем логгер запросов ДО ОБРАБОТЧИКОВ РОУТОВ
 app.use(requestLogger);
