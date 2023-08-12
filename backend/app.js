5c694a423af08233b5fd1c25e48713f3ea99f359
Igor Bibikov <igb604@yandex.ru>
2023-08-12 12:21:17 +0500

    фронт+бэк 6

--- backend/app.js
+++ backend/app.js
@@ -33,8 +33,8 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:4000",
-      "http://api.igorb.nomoreparties.co",
-      "https://api.igorb.nomoreparties.co",
+      "http://igorb.nomoreparties.co",
+      "https://igorb.nomoreparties.co",
     ],
     credentials: true,
   })
