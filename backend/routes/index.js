22a821a0fdc7016cbde6d08099cf40a35e7f06e0
Igor Bibikov <igb604@yandex.ru>
2023-08-12 13:38:06 +0500

    фронт+бэк 12

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const NotFoundErr = require("../errors/NotFoundErr");
-const auth = require("../middlewares/auth");
+const express = require('express');
+const NotFoundErr = require('../errors/NotFoundErr');
+const auth = require('../middlewares/auth');
 
 const routes = express.Router();
 const {
   ValidationСreateUser,
   ValidationLogin,
-} = require("../middlewares/validation");
+} = require('../middlewares/validation');
 
-const { login, createUser, logout } = require("../controllers/users");
+const { login, createUser, logout } = require('../controllers/users');
 
-const { usersRoutes } = require("./users");
-const { cardsRoutes } = require("./cards");
+const { usersRoutes } = require('./users');
+const { cardsRoutes } = require('./cards');
 
 // Краш тест
-routes.get("/crash-test", () => {
+routes.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Сервер сейчас упадёт");
+    throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
 
-routes.post("/signin", ValidationLogin, login);
-routes.post("/signout", logout);
-routes.post("/signup", ValidationСreateUser, createUser);
+routes.post('/signin', ValidationLogin, login);
+routes.post('/signout', logout);
+routes.post('/signup', ValidationСreateUser, createUser);
 
 routes.use(auth);
 
-routes.use("/users", usersRoutes);
-routes.use("/cards", cardsRoutes);
+routes.use('/users', usersRoutes);
+routes.use('/cards', cardsRoutes);
 
-routes.use("/", (req, res, next) => {
-  next(new NotFoundErr("Неверный путь."));
+routes.use('/', (req, res, next) => {
+  next(new NotFoundErr('Неверный путь.'));
 });
 
 module.exports = { routes };
